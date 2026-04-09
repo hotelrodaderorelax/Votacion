e705e98c3d09b67a3aedff1e3847deefaef7b082
hotelrodaderorelax <hotelrodaderorelaxrecepcion@gmail.com>
2026-04-08 21:16:01 -0500

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,6 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  optimizeFonts: false, 
+};
+
+export default nextConfig;
